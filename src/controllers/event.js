334bce1d586f2632b3394d3115bab011057e11f0
Abdullah Momoh <momohabdullah20@gmail.com>
2022-10-02 15:28:47 +0100

    uodated dev

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,42 +1,185 @@
 "use strict";
 
+const { response } = require("express");
 const { Op } = require("sequelize");
-const { Event } = require("../models");
+const { Event, Price } = require("../models");
+const { asyncForEach } = require("../util");
 const logger = require("../util/log");
 
-const createEvent = async (req, res) => {
-  const data = await Event.create({
-    user_id: req.isce_auth.user_id,
-    image: req.body.image,
-    title: req.body.title,
-    description: req.body.description,
-    start_date: req.body.start_date,
-    end_date: "2022-04-10"
+const totalCount = async () => {
+  let val = await Event.count({
+    where: {
+      start_date: {
+        [Op.lt]: new Date(),
+      },
+    },
+  })
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => logger(err));
+  return val;
+};
+
+const getPrices = async (id) => {
+  return await Price.findAll({ where: { event_id: id } }).then((data) => {
+    data = data.length ? data : [];
+    return data;
   });
-  if(!data){
-    res.json({ success: 'false', message: 'Unable to save event' })
+  // .catch((err) => logger(err));
+};
+
+const getPast = async () => {
+  return new Promise(async (resolve) => {
+    await Event.findAll({
+      limit: 20,
+      where: {
+        start_date: {
+          [Op.lt]: new Date(),
+        },
+      },
+    }).then(async (data) => {
+      let events = [];
+      await asyncForEach(data, async (item) => {
+        await getPrices(item.id).then((prices) => {
+          events.push({ ...item, prices });
+        });
+      }).finally(() => {
+        resolve(events);
+      });
+    });
+  });
+};
+
+const getFuture = async () => {
+  return new Promise(async (resolve) => {
+    await Event.findAll({
+      limit: 20,
+      where: {
+        start_date: {
+          [Op.gt]: new Date(),
+        },
+      },
+    }).then(async (data) => {
+      let events = [];
+      await asyncForEach(data, async (item) => {
+        await getPrices(item.id).then((prices) => {
+          events.push({ ...item, prices });
+        });
+      }).finally(() => {
+        resolve(events);
+      });
+    });
+  });
+};
+
+const createEvent = async (req, res) => {
+  /*
+    #swagger.tags = ["Event"]
+    #swagger.description = 'Create Event'
+    #swagger.security = [{
+               "apikey": []
+        }]
+    #swagger.parameters['obj'] = {
+                in: 'body',
+                schema: {
+                      $title: "string",
+                      $description: "string",
+                      $image: "string",
+                      $location: "string",
+                      $user_id: "string",
+                      $start_date: "datetime",  
+                      $end_date: "datetime",  
+                }
+        }
+  */
+
+  try {
+    const data = await Event.create({
+      user_id: req.isce_auth.user_id,
+      image: req.body.image,
+      title: req.body.title,
+      location: req.body.location,
+      description: req.body.description,
+      start_date: req.body.start_date,
+      end_date: "2022-10-10",
+    });
+
+    let response;
+    if (data.id) {
+      response = {
+        success: "true",
+        message: "Event created successfully",
+        data,
+      };
+    } else {
+      response = { success: "false", message: "Unable to save event" };
+    }
+    res.send(response);
+  } catch (error) {
+    logger(error);
+    res.send({ success: "false", message: error.message });
   }
-  res.json({ success: 'true', message: 'Event created successfully', data: req.body });
 };
 
 const updateEvent = async (req, res) => {
-  const data = await Event.update(req.body, {
-    where: {
-      id: req.body.id,
-    },
-  });
-  if(!data){
-    res.json({
-      success: 'false',
-      message: 'Unable to update event'
-    });
-  }
+  /*
+    #swagger.tags = ["Event"]
+    #swagger.description = 'Update Event'
+     #swagger.security = [{
+               "apikey": []
+        }]
+     #swagger.parameters['obj'] = {
+                in: 'body',
+                schema: {
+                      $title: "string",
+                      $description: "string",
+                      $image: "string",
+                      $location: "string",
+                      $start_date: "datetime",  
+                      $end_date: "datetime",  
+                }
+        }
+  */
 
-  res.json({
-    success: 'true',
-    message: 'Event updated successfully!',
-    data,
-  });
+  await Event.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      res.send({
+        success: true,
+        data,
+      });
+    })
+    .catch((err) => logger(err));
+};
+
+const deleteEvent = async (req, res) => {
+  /*
+    #swagger.tags = ["Event"]
+    #swagger.description = 'Delete event by id'
+     #swagger.security = [{
+               "apikey": []
+        }]
+  */
+
+  await Event.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => {
+      res.send({
+        success: true,
+        message: "Event deleted",
+      });
+    })
+    .catch((err) => {
+      logger(err)
+      res.send({ success: "false", message: error.message });
+    });
 };
 
 const getEvents = async (req, res) => {
@@ -55,22 +198,143 @@ const getEvents = async (req, res) => {
     offset = offset - limit;
   }
 
-  const all_events = await Event.findAll({ limit, offset });
-  if(!all_events){
-    res.json({ 
-      success: 'false', 
-      message: 'Unable to retrieve data'
-    })
-  }
+  await totalCount()
+    .then(async (total) => {
+      await Event.findAll({
+        limit,
+        offset,
+        where: {
+          start_date: {
+            [Op.gte]: new Date(),
+          },
+        },
+      }).then(async (data) => {
+        let past, upcoming;
 
-  res.json({
-    success: 'true',
-    message: 'Data retrieved successfully',
-    data: all_events
-  })
+        await getPast().then((pe) => {
+          past = pe;
+        });
+
+        await getFuture().then((fe) => {
+          upcoming = fe;
+        });
+        let events = [];
+        await asyncForEach(data, async (item) => {
+          await getPrices(item.id).then((prices) => {
+            events.push({ ...item, prices });
+          });
+        }).finally(() => {
+          res.send({
+            success: "true",
+            data: {
+              count: data.length,
+              all: data,
+              past,
+              upcoming,
+              total,
+            },
+          });
+        });
+      });
+    })
+    .catch((err) => {
+      logger(err);
+      res.send({
+        success: "false",
+        message: "Unable to get events list",
+      });
+    });
 };
 
-const getEvent = async (req, res) => {
+const getPastEvents = async (req, res) => {
+  /*
+    #swagger.tags = ["Event"]
+    #swagger.description = 'Get past events'
+     #swagger.security = [{
+               "apikey": []
+        }]
+  */
+  let offset = 0,
+    page = Number(req.query.page) || 1,
+    limit = Number(req.query.limit) || 100;
+  if (page > 1) {
+    offset = limit * page;
+    offset = offset - limit;
+  }
+
+  await totalCount()
+    .then(async (total) => {
+      await Event.findAll({
+        limit,
+        offset,
+        where: {
+          start_date: {
+            [Op.lt]: new Date(),
+          },
+        },
+      }).then(async (data) => {
+        let events = [];
+        await asyncForEach(data, async (item) => {
+          await getPrices(item.id).then((prices) => {
+            events.push({ ...item, prices });
+          });
+        }).finally(() => {
+          res.send({
+            success: "true",
+            data: {
+              count: data.length,
+              all: data,
+              total,
+            },
+          });
+        });
+        res.send({ success: true, data, total });
+      });
+    })
+    .catch((err) => logger(err));
+};
+
+const searchEvents = async (req, res) => {
+  /*
+    #swagger.tags = ["Event"]
+    #swagger.description = 'Search events using title or description'
+     #swagger.security = [{
+               "apikey": []
+        }]
+  */
+  let offset = 0,
+    page = Number(req.query.page) || 1,
+    limit = Number(req.query.limit) || 100,
+    query = req.query.query;
+
+  if (page > 1) {
+    offset = limit * page;
+    offset = offset - limit;
+  }
+
+  await totalCount()
+    .then(async (total) => {
+      await Event.findAll({
+        limit,
+        offset,
+        where: {
+          [Op.or]: [
+            { title: { [Op.like]: query } },
+            { description: { [Op.like]: query } },
+          ],
+          title: { [Op.like]: query },
+          start_date: {
+            [Op.gte]: new Date(),
+          },
+        },
+      }).then((data) => {
+        res.send({ success: true, data, total });
+      });
+    })
+    .catch((err) => logger(err));
+};
+
+const getEvent = (req, res) => {
   /*
     #swagger.tags = ["Event"]
     #swagger.description = 'Get Event by id'
@@ -79,17 +343,26 @@ const getEvent = async (req, res) => {
         }]
   */
 
-  const data = await Event.findOne({ where: { id: req.body.id } });
-  if (!data) {
-    res.json({ success: 'false', message: 'Unable to get event!' });
-  } 
-
-  res.json({ success: 'true', message: "Event retrieved successfully!" });
+  Event.findOne({ where: { id: req.params.id } })
+    .then(async (data) => {
+      if (data) {
+        await getPrices(data.id).then((prices) => {
+          data.prices = prices;
+          res.send({ success: true, data });
+        });
+      } else {
+        res.send({ success: false, message: "No data" });
+      }
+    })
+    .catch((err) => logger(err));
 };
 
 module.exports = {
   createEvent,
   updateEvent,
+  deleteEvent,
   getEvents,
+  getPastEvents,
+  searchEvents,
   getEvent,
 };
