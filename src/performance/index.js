84733f584596e8e8ba2ca16ac09196f97f72ed20
Prince <38994284+ninjamovesonly@users.noreply.github.com>
2023-03-13 13:54:10 +0100

    fix: dynamically generate relevant base url if environment is either development or production

--- src/performance/index.js
+++ src/performance/index.js
@@ -1,6 +1,11 @@
 const loadtest = require("loadtest");
+
+const BASE_URL =
+  process.env.NODE_ENV === "development" ?
+    "http://localhost:3000" : "https://api.isce.app"; // insert correct production api base url
+
 const options = {
-  url: "http://localhost:3000/api/events?page=1&limit=50",
+  url: `${BASE_URL}/api/events?page=1&limit=50`,
   maxRequests: 1000,
 };
 loadtest.loadTest(options, function (error, result) {
